350875c5aacbebd16ac43b2cdfa47e691c162d06
Dan Bikle <danbikle@outlook.com>
2017-07-02 18:08:44 -0700

    more

--- curlem.bash
+++ curlem.bash
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-curl localhost:5010/skservice/SPY/2016/25
-curl localhost:5010/keras/SPY/2016/25?features=pctlag1,pctlag2,slope2,slope4,dow,moy
-curl localhost:5010/db/SPY/2016/25?features=pctlag1,pctlag2,slope2,slope4,dow,moy
-curl localhost:5010/dbkeras/SPY/2016/25?features=pctlag1,pctlag2,slope2,slope4,dow,moy
+/usr/bin/curl localhost:5010/skservice/SPY/2016/25
+/usr/bin/curl localhost:5010/keras/SPY/2016/25?features=pctlag1,pctlag2,slope2,slope4,dow,moy
+/usr/bin/curl localhost:5010/db/SPY/2016/25?features=pctlag1,pctlag2,slope2,slope4,dow,moy
+/usr/bin/curl localhost:5010/dbkeras/SPY/2016/25?features=pctlag1,pctlag2,slope2,slope4,dow,moy
